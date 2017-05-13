5d3691f951d998e8e97304cc42d83629565684f7
Steven Scaffidi <sscaff1@gmail.com>
2017-05-12 20:11:03 -0700

    making it more SEO friendly

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Menu from './Menu';
 import { BACKGROUND_IMAGE } from '../constants';
+const BASE_URL = 'https://nolaword.com'
 
 function properCase(str) {
   const prepend = 'New Orleans News Fast';
@@ -12,6 +13,15 @@ function properCase(str) {
   return prepend + ' | ' + firstLetter + str.substr(2);
 }
 
+function getMetaDescription(str) {
+  if (str === '/') {
+    return 'NolaWord provides New Orleans news around the clock. Get the news that you care about fast on all your devices.'
+  } else if (str === '/sports') {
+    return 'NolaWord gives you news about your favorite New Orleans sports team. Saints, Pelicans, BabyCakes, LSU, and Tulane sports news on all your devices.'
+  }
+  return 'NolaWord provides the weather that you care about fast. Check us out to get the fastest weather updates you can get.'
+}
+
 export default function Layout({ url, children }) {
   return (
     <div className="container">
@@ -19,6 +29,8 @@ export default function Layout({ url, children }) {
         <title>{properCase(url.pathname)}</title>
         <link href="https://fonts.googleapis.com/css?family=Open+Sans|Rubik+Mono+One" rel="stylesheet" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={getMetaDescription(url.pathname)} />
+        <link rel="canonical" href={`${BASE_URL}${url.pathname}`} />
         <script dangerouslySetInnerHTML={{
           __html: `
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
