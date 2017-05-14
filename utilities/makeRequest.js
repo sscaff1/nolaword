d3c9a5a495d68f02c1a143b3bf1a8309814e5af1
Steven Scaffidi <sscaff1@gmail.com>
2017-05-14 11:23:20 -0700

    varying products

--- utilities/makeRequest.js
+++ utilities/makeRequest.js
@@ -1,6 +1,17 @@
 import fetch from 'isomorphic-unfetch';
 const config = require('../config/config.json');
 
+function getPosition(base = 0) {
+  return Math.ceil(Math.random() * 4) + base;
+}
+
+function getProducts() {
+  const products = [...config.products];
+  const first = products.splice(Math.floor(Math.random() * products.length), 1);
+  const second = products.splice(Math.floor(Math.random() * products.length), 1);
+  return [...first, ...second];
+}
+
 export default function makeRequest(host, newsDesk, page) {
   return fetch(`${host}/api/articles?newsDesk=${newsDesk}&page=${page}`)
   .then(response => response.json())
@@ -9,8 +20,11 @@ export default function makeRequest(host, newsDesk, page) {
       console.log('out of docs');
       return [];
     }
-    docs.splice(4, 0, config.products[0]);
-    docs.splice(8, 0, config.products[0]);
+    const position1 = getPosition();
+    const position2 = getPosition(5);
+    const [product1, product2] = getProducts();
+    docs.splice(position1, 0, product1);
+    docs.splice(position2, 0, product2);
     return docs;
   })
   .catch(error => console.log(error))
